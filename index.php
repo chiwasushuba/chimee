568ac4d712606571a00b2645dd91727f7e0040bd
Chimee <arimatheac.suarez@gmail.com>
2023-08-22 12:21:38 +0800

    fixed #1

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <li><a href="blog.php"> Blog </a></li>
 <li><a href="contact.php"> Contact </a></li>
 
-<h1> Welcome to our website! </h1>
+<h1> Welcomes to our website! </h1>
 <p>Explore a world of possibilities as you navigate through our diverse range of content. <br>
 From insightful articles to captivating visuals, our index page is your gateway to a wealth of information and inspiration. <br>
 Whether you're seeking knowledge, entertainment, or a fresh perspective, we've got something special in store for you.<br>
